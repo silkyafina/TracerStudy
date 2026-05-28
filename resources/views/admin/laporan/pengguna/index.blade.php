51ab05585e2f9181fa92dec0a4495c31de71bab1
DataAreaa <aafnsly@gmail.com>
2026-05-28 21:41:21 +0700

    laporan pengguna lulusan

--- resources/views/admin/laporan/pengguna/index.blade.php
+++ resources/views/admin/laporan/pengguna/index.blade.php
@@ -125,42 +125,7 @@
             </table>
         </div>
         </div>
-        @if($jenis == 'saran')
-
-        <div class="table-responsive">
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th>No</th>
-                        <th>Nama Alumni</th>
-                        <th>Tahun Lulus</th>
-                        <th>Perusahaan</th>
-                        <th>Saran & Masukan</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @forelse($data as $item)
-                        <tr>
-                            <td>{{ $loop->iteration }}</td>
-                            <td>{{ $item->nama_lengkap }}</td>
-                            <td>{{ $item->tahun_lulus }}</td>
-                            <td>{{ $item->nama_perusahaan }}</td>
-                            <td style="white-space: normal;">
-                                {{ $item->saran }}
-                            </td>
-                        </tr>
-                    @empty
-                        <tr>
-                            <td colspan="5" class="text-center">
-                                Tidak ada data saran
-                            </td>
-                        </tr>
-                    @endforelse
-                </tbody>
-            </table>
-        </div>
-        
-        @endif
+       
 
     {{-- CHART JUMLAH --}}
     <div class="card shadow-sm">
@@ -238,7 +203,42 @@
     </div>
 
     @endif
+    @if($jenis == 'saran')
 
+    <div class="table-responsive">
+        <table class="table table-bordered">
+            <thead>
+                <tr>
+                    <th>No</th>
+                    <th>Nama Alumni</th>
+                    <th>Tahun Lulus</th>
+                    <th>Perusahaan</th>
+                    <th>Saran & Masukan</th>
+                </tr>
+            </thead>
+            <tbody>
+                @forelse($data as $item)
+                    <tr>
+                        <td>{{ $loop->iteration }}</td>
+                        <td>{{ $item->nama_lengkap }}</td>
+                        <td>{{ $item->tahun_lulus }}</td>
+                        <td>{{ $item->nama_perusahaan }}</td>
+                        <td style="white-space: normal;">
+                            {{ $item->saran }}
+                        </td>
+                    </tr>
+                @empty
+                    <tr>
+                        <td colspan="5" class="text-center">
+                            Tidak ada data saran
+                        </td>
+                    </tr>
+                @endforelse
+            </tbody>
+        </table>
+    </div>
+    
+    @endif
 </div>
 @endsection
 
