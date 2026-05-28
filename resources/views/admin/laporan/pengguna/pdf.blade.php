1ff4571e5862fe43f1c52413693ed05ce86baa16
DataAreaa <aafnsly@gmail.com>
2026-05-28 22:18:49 +0700

    laporan pengguna lulusan

--- resources/views/admin/laporan/pengguna/pdf.blade.php
+++ resources/views/admin/laporan/pengguna/pdf.blade.php
@@ -162,7 +162,7 @@
         @foreach($data as $item)
         <tr>
             <td>{{ $loop->iteration }}</td>
-            <td>{{ $item->nama }}</td>
+            <td>{{ $item->nama_lengkap}}</td>
             <td>{{ $item->tahun_lulus }}</td>
             <td>{{ $item->nama_perusahaan }}</td>
             <td>{{ $item->saran }}</td>
